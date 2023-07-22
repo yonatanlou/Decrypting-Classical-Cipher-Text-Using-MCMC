1a6fd341a9b174bfcc84a45c43e8222ad248a2e2
Yonatan Lourie <yonatanlou@gmail.com>
2023-07-22 20:26:13 +0300

    reorg

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
-from cipher_decryption import run, set_seed
-import click
 
+from mcmc import run, set_seed
 
 set_seed(42)
 
 
 PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
-TEXT_FILE = "haaretz.txt"
+TEXT_FILE = "war-and-peace.txt"
 
 message_heb = "המלחמה ברצועת עזה נגמרה לגמרי ושלום עולמי קיים בארץ ישראל אף על פי כך, נדקר טיפוס אחד, המצב בשווקים הידרדר משמעותית בחודש האחרון בעקבות המצב החמור במושבה מאדים"
 message_eng = "What is important are the rights of man, emancipation from prejudices, and equality of citizenship, and all these ideas Napoleon has retained in full force"
 
-run(PATH, TEXT_FILE, "hebrew", message_heb, plot=False, iterations=150000)
+run(PATH, TEXT_FILE, "english", message_eng, plot=False, iterations=6500)
